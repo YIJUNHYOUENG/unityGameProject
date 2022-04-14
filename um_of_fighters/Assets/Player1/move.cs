51728361a1f10f6af169f3b5d5b49de53cb404fd
YIJUNHYOUENG <69065079+YIJUNHYOUENG@users.noreply.github.com>
2022-04-14 23:12:13 +0900

    20220414
    캐릭터 왼쪽 펀치 완성(타격 임펙트 제외), move 파일 주석 x,

--- um_of_fighters/Assets/Player1/move.cs
+++ um_of_fighters/Assets/Player1/move.cs
@@ -6,6 +6,8 @@ public class move : MonoBehaviour
 {
     private Rigidbody myRigid; // player 이동을 위한 변수
     public Animator animator;
+    public bool lp = false;
+    public bool mv = false;    
 
     void Start()
     {
@@ -19,30 +21,84 @@ public class move : MonoBehaviour
         float moveZ = 0f; // Z값 초기화
         float moveX = 0f; // X값 초기화
 
-        if (Input.GetKey(KeyCode.W)) // w를 눌렀을 때
-        {
+        RotatePlayer(new Vector3(0f, 180, 0f));
 
-        }      
+        if(!mv) { 
+            if (Input.GetKey(KeyCode.W)) // w를 눌렀을 때
+            {
 
-        if (Input.GetKey(KeyCode.S)) // s를 눌렀을 때
-        {
+            }
 
+            if (Input.GetKey(KeyCode.S)) // s를 눌렀을 때
+            {
+
+            }
+
+            if (Input.GetKey(KeyCode.A)) // a를 눌렀을 때
+            {
+                moveX += 0.2f;  // X축으로 증가
+                moveZ -= 0.2f;
+
+            }
+            else if (Input.GetKey(KeyCode.D))// d를 눌렀을 때
+            {
+                moveX -= 0.2f; // X축으로 감소
+                moveZ += 0.2f;
+            }
         }
 
-        if (Input.GetKey(KeyCode.A)) // a를 눌렀을 때
+        if (Input.GetKey(KeyCode.J))
         {
-            moveX += 0.2f;  // X축으로 증가
-            animator.SetBool("run", true);
-        } else if (Input.GetKey(KeyCode.D))// d를 눌렀을 때
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                mv = true; 
+                if (false == lp)
+                {
+                    animator.SetBool("punchL", true);
+                    lp = true;
+                }
+            }
+            else
+            {
+                lp = false;
+            }
+        }
+        else
         {
-            moveX -= 0.2f; // X축으로 감소
-            animator.SetBool("run", true);
-        } else
-        {
-            animator.SetBool("run", false);
+            animator.SetBool("punchL", false);
         }
 
-        transform.Translate(new Vector3(moveX, moveZ, 0f) * 0.1f); // 키보드를 눌렀을 때 움직이는 문장
+        if (Input.GetKey(KeyCode.K))
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                mv = true;
+                if (false == lp)
+                {
+                    animator.SetBool("punchR", true);
+                    lp = true;
+                }
+            }
+            else
+            {
+                lp = false;
+            }
+        }
+        else
+        {
+            animator.SetBool("punchR", false);
+        }
+
+        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punching(Left)"))
+        {
+            mv = false;
+        }
+
+        transform.Translate(new Vector3(moveX, 0f, moveZ) * 0.1f); // 키보드를 눌렀을 때 움직이는 문장
+    }
+
+    public void RotatePlayer(Vector3 rotation)
+    {
+        transform.rotation = Quaternion.Euler(rotation);
     }
 }
-
