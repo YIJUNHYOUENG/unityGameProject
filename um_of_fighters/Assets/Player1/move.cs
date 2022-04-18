2697e9ac82fa0cd08fb71a79ae4494451963409e
YIJUNHYOUENG <69065079+YIJUNHYOUENG@users.noreply.github.com>
2022-04-18 22:13:44 +0900

    2022_04_18
    유니티 캐릭터 애니메이션 전체적으로 바꿈, 서있는 것과 움직임은 재현, 이제 타격 이벤트 추가 예정

--- um_of_fighters/Assets/Player1/move.cs
+++ um_of_fighters/Assets/Player1/move.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     private Rigidbody myRigid; // player 이동을 위한 변수
-    public Animator animator;
+    public Animator animator; // animation 사용을 위한 변수
     public bool lp = false;
     public bool mv = false;    
 
     void Start()
     {
         myRigid = GetComponent<Rigidbody>(); // player 이동을 위한 초기 설정
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); // animation 사용을 위한 초기 설정
     }
 
     void Update()
     {
 
         float moveZ = 0f; // Z값 초기화
-        float moveX = 0f; // X값 초기화
 
         RotatePlayer(new Vector3(0f, 180, 0f));
 
@@ -36,14 +35,16 @@ public class move : MonoBehaviour
 
             if (Input.GetKey(KeyCode.A)) // a를 눌렀을 때
             {
-                moveX += 0.2f;  // X축으로 증가
-                moveZ -= 0.2f;
-
+                moveZ -= 0.2f;  // x축으로 감소
+                animator.SetBool("run", true); // Walking 애니메이션 실행
             }
             else if (Input.GetKey(KeyCode.D))// d를 눌렀을 때
             {
-                moveX -= 0.2f; // X축으로 감소
-                moveZ += 0.2f;
+                moveZ += 0.2f; // Z축으로 증가
+                animator.SetBool("run", true); // Walking 애니메이션 실행
+            } else
+            {
+                animator.SetBool("run", false); // Walking 애니메이션 종료와 Ready Idle 애니메이션 실행
             }
         }
 
@@ -68,33 +69,13 @@ public class move : MonoBehaviour
             animator.SetBool("punchL", false);
         }
 
-        if (Input.GetKey(KeyCode.K))
-        {
-            if (Input.GetKeyDown(KeyCode.K))
-            {
-                mv = true;
-                if (false == lp)
-                {
-                    animator.SetBool("punchR", true);
-                    lp = true;
-                }
-            }
-            else
-            {
-                lp = false;
-            }
-        }
-        else
-        {
-            animator.SetBool("punchR", false);
-        }
-
+        
         if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punching(Left)"))
         {
             mv = false;
         }
 
-        transform.Translate(new Vector3(moveX, 0f, moveZ) * 0.1f); // 키보드를 눌렀을 때 움직이는 문장
+        transform.Translate(new Vector3(0f, 0f, moveZ) * 0.1f); // 키보드를 눌렀을 때 움직이는 문장
     }
 
     public void RotatePlayer(Vector3 rotation)
