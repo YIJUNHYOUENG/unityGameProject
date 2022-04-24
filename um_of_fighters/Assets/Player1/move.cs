2aa07f2c202e6e1ca2f336cf2125e8deb99924e8
YIJUNHYOUENG <69065079+YIJUNHYOUENG@users.noreply.github.com>
2022-04-24 13:34:02 +0900

    2022_04_23
    배경 삽입, 바닥 설정, 애니메이션 추가

--- um_of_fighters/Assets/Player1/move.cs
+++ um_of_fighters/Assets/Player1/move.cs
@@ -7,12 +7,14 @@ public class move : MonoBehaviour
     private Rigidbody myRigid; // player 이동을 위한 변수
     public Animator animator; // animation 사용을 위한 변수
     public bool lp = false;
-    public bool mv = false;    
+    public bool mv = false;
+    Vector3 pos;
 
     void Start()
     {
         myRigid = GetComponent<Rigidbody>(); // player 이동을 위한 초기 설정
         animator = GetComponent<Animator>(); // animation 사용을 위한 초기 설정
+        pos = this.gameObject.transform.position;
     }
 
     void Update()
@@ -22,9 +24,12 @@ public class move : MonoBehaviour
 
         RotatePlayer(new Vector3(0f, 180, 0f));
 
+
+        
             if (Input.GetKey(KeyCode.W)) // w를 눌렀을 때
             {
-
+                animator.SetBool("jump", true);
+                Debug.Log("jump");
             }
 
             if (Input.GetKey(KeyCode.S)) // s를 눌렀을 때
@@ -32,37 +37,39 @@ public class move : MonoBehaviour
 
             }
 
-            if (Input.GetKey(KeyCode.A)) // a를 눌렀을 때
+            if (Input.GetKey(KeyCode.A)) // a를 눌렀을 때    
             {
                 moveZ -= 0.2f;  // x축으로 감소
                 animator.SetBool("run", true); // Walking 애니메이션 실행
+                pos = this.gameObject.transform.position;
             }
             else if (Input.GetKey(KeyCode.D))// d를 눌렀을 때
             {
                 moveZ += 0.2f; // Z축으로 증가
                 animator.SetBool("run", true); // Walking 애니메이션 실행
+                pos = this.gameObject.transform.position;
             } else
             {
                 animator.SetBool("run", false); // Walking 애니메이션 종료와 Ready Idle 애니메이션 실행
-            }
+                animator.SetBool("jump", false);
+                mv = false;
+                transform.position = new Vector3(pos.x, pos.y, pos.z);
+           
+        }
 
         if (Input.GetKey(KeyCode.J))
         {
             if (Input.GetKeyDown(KeyCode.J))
             {
-                mv = true; 
+                pos = this.gameObject.transform.position;
+                RotatePlayer(new Vector3(0f, 170, 0) * Time.deltaTime);
                 animator.SetBool("punchL", true);
+                Debug.Log("punch");
             }
         }
         else
         {
             animator.SetBool("punchL", false);
-        }
-
-        
-        if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("Punching(Left)"))
-        {
-            mv = false;
         }
 
         transform.Translate(new Vector3(0f, 0f, moveZ) * 0.1f); // 키보드를 눌렀을 때 움직이는 문장
@@ -71,5 +78,13 @@ public class move : MonoBehaviour
     public void RotatePlayer(Vector3 rotation)
     {
         transform.rotation = Quaternion.Euler(rotation);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Capsule"))
+        {
+            Debug.Log("1");
+        }
     }
 }
